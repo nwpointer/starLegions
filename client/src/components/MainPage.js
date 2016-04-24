c13ce224aea7d087536797848352e9ebe1522f75
Michael A Tomcal <michael@twoeyetech.com>
2016-04-23 18:14:53 -0700

    More terminal features

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,10 +1,68 @@
 import React from 'react';
 import Layout from './Layout';
 
+const CommandInput = React.createClass({
+  defaultValue: `Type command...`,
+  onInputBlur(e) {
+    var value = this.refs.command.value;
+    if (value === this.defaultValue) {
+      this.refs.command.value = '';
+    } else {
+      this.refs.command.value = this.defaultValue;
+    }
+  },
+  componentDidMount() {
+
+  },
+  render() {
+    return (
+      <div>&gt;<input ref="command" type="text" onClick={this.onInputBlur} onBlur={this.onInputBlur} defaultValue={this.defaultValue}/></div>
+    );
+  }
+});
+
 const MainBody = React.createClass({
+  fixScroll() {
+    var {terminal} = this.refs;
+    if (terminal) {
+      setTimeout(() => {
+        terminal.scrollTop = terminal.scrollHeight;
+      }, 200);
+    }
+  },
+  componentDidMount() {
+    this.fixScroll();
+  },
+  componentDidUpdate() {
+    this.fixScroll();
+  },
   render() {
     return <div>
-      <div className="terminal">
+      <div ref="terminal" className="terminal">
+        <p>You see a dark forbidding planet of <span className="highlight">pure evil.</span></p>
+        <div className="description">
+          <p>Your scans show:</p>
+          <ul>
+            <li>An Asteroid belt</li>
+            <li>Mineral Rich Moon</li>
+          </ul>
+        </div>
+        <p>You see a dark forbidding planet of <span className="highlight">pure evil.</span></p>
+        <div className="description">
+          <p>Your scans show:</p>
+          <ul>
+            <li>An Asteroid belt</li>
+            <li>Mineral Rich Moon</li>
+          </ul>
+        </div>
+        <p>You see a dark forbidding planet of <span className="highlight">pure evil.</span></p>
+        <div className="description">
+          <p>Your scans show:</p>
+          <ul>
+            <li>An Asteroid belt</li>
+            <li>Mineral Rich Moon</li>
+          </ul>
+        </div>
         <p>You see a dark forbidding planet of <span className="highlight">pure evil.</span></p>
         <div className="description">
           <p>Your scans show:</p>
@@ -14,7 +72,7 @@ const MainBody = React.createClass({
           </ul>
         </div>
       </div>
-      <div>&gt;<input type="text" defaultValue={`Type command...`}/></div>
+      <CommandInput />
     </div>;
   }
 });
