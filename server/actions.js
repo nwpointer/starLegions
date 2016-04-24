31a3695760cee777a80baff7367759d7bb74eb2d
admin <admin@Jamess-MacBook-Pro.local>
2016-04-23 18:05:33 -0700

    playing arround with state obj

--- server/actions.js
+++ server/actions.js
@@ -2,10 +2,6 @@ var {pointDistance, point} = require('./util.js');
 
 module.exports = function(state){
 	return {
-		attack : function(data){
-			state.health -= data.str;
-			return state
-		},
 		position: function(data){
 			return point(state.players[data.id])
 		},
--- server/state.js
+++ server/state.js
@@ -1,22 +1,62 @@
 {
-	"health": 100,
 	"players": [
 		{
 			"x":0,
 			"y":20,
-			"name": "geb"
+			"name": "geb",
+			"crew":[]
 		}
 	],
+	"bandits":[
+		{
+			"x":1000,
+			"y":20,
+			"name": "geb's dad"
+		}
+	],
+	"resources":{
+		"1":{
+			"name": "plutoneoum"
+		},
+		"4":{
+			"name":"iron"
+		}
+	},
 	"systems": [
 		{
 			"x":0,
 			"y":10,
-			"name": "A-5 beta"
+			"name": "A-5 beta",
+			"planets":{
+				"earth":{
+					"population": 30000,
+					"resources": {
+						"1": 1000,
+						"4": 2000
+					}
+				}
+			}
 		},
 		{
 			"x":100,
 			"y":10,
-			"name": "The Horizon"
+			"name": "The Horizon",
+			"planets":[]
 		}
-	]
+	],
+	"astroids":[],
+	"ships":[
+		{
+			"name":"cargo hold",
+			"speed":10,
+			"warpSpeed":100,
+			"fuel":100,
+			"attack":10,
+			"defence":100,
+			"capacity":100,
+			"parts":[]
+		}
+	],
+	"shipsParts":[],
+	"randomEvents":[],
 }
